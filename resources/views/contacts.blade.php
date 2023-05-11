bc8a439d3c1cf9bc6c129eb94b5373f324a6968d
alexvsezanyato <angelo@osradar.com>
2023-05-11 21:56:12 +0700

    Add blue background to contacts

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -12,41 +12,43 @@
 <h1>Контакты</h1>
 
 <div class="contacts">
-    <div class="row">
-        <div class="item">
-            <div class="description">
-                <span class="description-item description-title"><span class="icon"><i class="fa-solid fa-phone"></i></span><span>Номера телефонов</span></span>
-                <span class="description-item"><a href="tel:89963900000" class="phone-number">8 (996) 390-00-00</a></span>
-                <span class="description-item"><a href="tel:89963900000" class="phone-number">8 (996) 390-00-00</a></span>
+    <div class="cards">
+        <div class="row">
+            <div class="item">
+                <div class="description">
+                    <span class="description-item description-title"><span class="icon"><i class="fa-solid fa-phone"></i></span><span>Номера телефонов</span></span>
+                    <span class="description-item"><a href="tel:89963900000" class="phone-number">8 (996) 390-00-00</a></span>
+                    <span class="description-item"><a href="tel:89963900000" class="phone-number">8 (996) 390-00-00</a></span>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="description">
+                    <span class="description-item description-title"><span class="icon"><i class="fa-solid fa-envelope"></i></span><span>Электронная почта</span></span>
+                    <span class="description-item"><a href="tel:89963900000" class="phone-number">alo-alo@gmail.com</a></span>
+                    <span class="description-item"><a href="tel:89963900000" class="phone-number">alo-alo@yandex.ru</a></span>
+                </div>
             </div>
         </div>
 
-        <div class="item">
-            <div class="description">
-                <span class="description-item description-title"><span class="icon"><i class="fa-solid fa-envelope"></i></span><span>Электронная почта</span></span>
-                <span class="description-item"><a href="tel:89963900000" class="phone-number">alo-alo@gmail.com</a></span>
-                <span class="description-item"><a href="tel:89963900000" class="phone-number">alo-alo@yandex.ru</a></span>
+        <div class="row">
+            <div class="item">
+                <div class="description">
+                    <span class="description-item description-title"><span class="icon"><i class="fa-solid fa-hashtag"></i></span><span>Соц. сети</span></span>
+                    <span class="description-item"><a href="tel:89963900000" class="phone-number">Ссылка на Telegram</a></span>
+                    <span class="description-item"><a href="tel:89963900000" class="phone-number">Ссылка на Whatsapp</a></span>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="description">
+                    <span class="description-item description-title"><span class="icon"><i class="fa-solid fa-calendar-days"></i></span><span>График работы</span></span>
+                    <span class="description-item">Пн.-Пт. 8-17</span>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="row">
-        <div class="item">
-            <div class="description">
-                <span class="description-item description-title"><span class="icon"><i class="fa-solid fa-hashtag"></i></span><span>Соц. сети</span></span>
-                <span class="description-item"><a href="tel:89963900000" class="phone-number">Ссылка на Telegram</a></span>
-                <span class="description-item"><a href="tel:89963900000" class="phone-number">Ссылка на Whatsapp</a></span>
-            </div>
-        </div>
-
-        <div class="item">
-            <div class="description">
-                <span class="description-item description-title"><span class="icon"><i class="fa-solid fa-calendar-days"></i></span><span>График работы</span></span>
-                <span class="description-item">Пн.-Пт. 8-17</span>
-            </div>
-        </div>
-    </div>
-
-    <iframe class="map" src="https://yandex.ru/map-widget/v1/?scroll=false&um=constructor%3A356bea7be17be57ef30a764bd5f02be68c576674b6dedd99cf73dc26e3255109&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>
+    <iframe class="map" style="pointer-events: none;" src="https://yandex.ru/map-widget/v1/?scroll=false&um=constructor%3A356bea7be17be57ef30a764bd5f02be68c576674b6dedd99cf73dc26e3255109&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>
 </div>
 @endsection
