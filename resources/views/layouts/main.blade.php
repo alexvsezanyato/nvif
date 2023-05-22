fa66d4fd1a71bb4657851cc18ee283e0ee5a5685
alexvsezanyato <angelo@osradar.com>
2023-05-22 23:29:14 +0700

    Revision of themes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,7 +13,7 @@
 
         <link href="/css/general.css" rel="stylesheet">
         <link href="/css/header.css" rel="stylesheet">
-        <link href="/css/menu.css" rel="stylesheet">
+        <link href="/css/main-menu.css" rel="stylesheet">
         <link href="/css/submenu.css" rel="stylesheet">
         <link href="/css/breadcrumbs.css" rel="stylesheet">
         <link href="/css/plain-request-form.css" rel="stylesheet">
@@ -24,13 +24,14 @@
         <link href="/css/white-theme/submenu.css" rel="stylesheet">
         <link href="/css/white-theme/general.css" rel="stylesheet">
         <link href="/css/white-theme/breadcrumbs.css" rel="stylesheet">
+        <link href="/css/white-theme/plain-request-form.css" rel="stylesheet">
         <link href="/css/white-theme/footer.css" rel="stylesheet">
 
         <!--<link href="/css/dark-theme/general.css" rel="stylesheet">
         <link href="/css/dark-theme/submenu.css" rel="stylesheet">
         <link href="/css/dark-theme/breadcrumbs.css" rel="stylesheet">
         <link href="/css/dark-theme/header.css" rel="stylesheet">
-        <link href="/css/dark-theme/menu.css" rel="stylesheet">
+        <link href="/css/dark-theme/main-menu.css" rel="stylesheet">
         <link href="/css/dark-theme/plain-request-form.css" rel="stylesheet">
         <link href="/css/dark-theme/footer.css" rel="stylesheet">-->
 
@@ -41,48 +42,50 @@
         <div class="wrapper">
             <div class="top">
                 <header>
-                    <div class="wrapper">
-                        <div class="row general">
-                            <div class="row">
-                                <img src="/images/logo.webp" class="logo" alt="logo">
-                                <span class="description">Достаквка угля и древесины</span>
+                    <div class="container">
+                        <div class="wrapper">
+                            <div class="row general">
+                                <div class="row">
+                                    <img src="/images/logo.webp" class="logo" alt="logo">
+                                    <span class="description">Достаквка угля и древесины</span>
+                                </div>
+
+                                <div class="row">
+                                    <span class="schedule">Пн.-Пт.</span>
+
+                                    <span class="whatsapp">
+                                        <a href="tel:89963900000" class="whatsapp-icon contact-icon icon"><i class="fa-brands fa-whatsapp"></i></a>
+                                    </span>
+
+                                    <span class="telegram">
+                                        <a href="tel:89963900000" class="telegram-icon contact-icon icon"><i class="fa-brands fa-telegram"></i></i></a>
+                                    </span>
+
+                                    <span class="phone">
+                                        <!--<a href="tel:89963900000" class="phone-icon icon"><i class="fa-solid fa-phone"></i></a>-->
+                                        <a href="tel:89963900000" class="phone-number">8 (996) 390-00-00</a>
+                                    </span>
+
+                                    <button class="order-call">Заказать звонок</button>
+
+                                    <span class="dark-mode">
+                                        <button class="icon dark-mode-icon"><i class="fa-solid fa-moon"></i></button>
+                                    </span>
+                                </div>
                             </div>
 
-                            <div class="row">
-                                <span class="schedule">Пн.-Пт.</span>
+                            <div class="row main-menu">
+                                <div class="link-list">
+                                    <div class="item"><a class="link" href="/">Главная</a></div>
+                                    <div class="item"><a class="link" href="/catalog">Каталог</a></div>
+                                </div>
 
-                                <span class="whatsapp">
-                                    <a href="tel:89963900000" class="whatsapp-icon contact-icon icon"><i class="fa-brands fa-whatsapp"></i></a>
-                                </span>
+                                <div class="link-list">
+                                    <div class="item contact-link"><a class="link" href="/contacts">Контакты</a></div>
+                                    <!--<li class="search"><i class="fa-solid fa-magnifying-glass"></i></li>-->
+                                </div>
 
-                                <span class="telegram">
-                                    <a href="tel:89963900000" class="telegram-icon contact-icon icon"><i class="fa-brands fa-telegram"></i></i></a>
-                                </span>
-
-                                <span class="phone">
-                                    <!--<a href="tel:89963900000" class="phone-icon icon"><i class="fa-solid fa-phone"></i></a>-->
-                                    <a href="tel:89963900000" class="phone-number">8 (996) 390-00-00</a>
-                                </span>
-
-                                <button class="order-call">Заказать звонок</button>
-
-                                <span class="dark-mode">
-                                    <button class="icon dark-mode-icon"><i class="fa-solid fa-moon"></i></button>
-                                </span>
                             </div>
-                        </div>
-
-                        <div class="row main-menu">
-                            <div class="link-list">
-                                <div class="item"><a class="link" href="/">Главная</a></div>
-                                <div class="item"><a class="link" href="/catalog">Каталог</a></div>
-                            </div>
-
-                            <div class="link-list">
-                                <div class="item contact-link"><a class="link" href="/contacts">Контакты</a></div>
-                                <!--<li class="search"><i class="fa-solid fa-magnifying-glass"></i></li>-->
-                            </div>
-
                         </div>
                     </div>
                 </header>
